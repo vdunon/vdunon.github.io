17ff49c04a4000b8bd238ab032d204879db7f52d
Valentin DUNON <valentin.dunon@epitech.eu>
2025-12-24 01:57:49 +0100

    feat: advanced interested section

--- app/sections/interested.tsx
+++ app/sections/interested.tsx
@@ -1,10 +1,20 @@
+import Button from "../components/button";
 import SectionTitle from "../components/section_title";
+import Social from "../components/social";
 
 export default function Interested()
 {
     return (
         <section className="pt-10">
             <SectionTitle title="Interested ?"/>
+            <SectionTitle title="Contact Me :"/>
+            <div className="flex gap-5">
+                <Social social="Github" link="https://github.com/Nostlix/"/>
+                <Social social="LinkedIn" link="https://www.linkedin.com/in/valentin-dunon/"/>
+                <Social social="Email" link="mailto:vdunon91@gmail.com"/>
+            </div>
+            <SectionTitle title="My Resume :"/>
+            <Button variant="secondary">Upload CV</Button>
         </section>
     )
 }
