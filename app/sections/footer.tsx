6ced329d91a8af6497436e450bf825de23f7f178
Valentin DUNON <valentin.dunon@epitech.eu>
2026-01-06 18:19:30 +0100

    feat: increased padding of footer

--- app/sections/footer.tsx
+++ app/sections/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer()
 {
     return (
-        <footer className="bg-(image:--gradient-main) w-1/1 py-3">
+        <footer className="bg-(image:--gradient-main) w-1/1 py-6">
             <p className="text-center text-(--background)">
                 Designed & Developed by Valentin DUNON
                 - Epitech Student
