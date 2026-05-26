efa6a77e2993c9afa840293d27fa11e5418d31fd
Valentin DUNON <valentin.dunon@epitech.eu>
2026-05-27 01:01:53 +0200

    fix: navbar prevent to click to elements behind it

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 
 export default function Navbar() {
-    return <nav className="sticky z-99 top-0 flex flex-row justify-between w-full p-5">
-        <a href="/">
+    return <nav className="sticky pointer-events-none z-99 top-0 flex flex-row justify-between w-full p-5">
+        <a href="/" className="pointer-events-auto">
           <Image
             src="/icons/logo_portfolio.png"
             alt="Logo"
@@ -12,7 +12,7 @@ export default function Navbar() {
             priority
         />
         </a>
-        <a href="https://www.epitech.eu/" target="blank">
+        <a href="https://www.epitech.eu/" target="blank" className="pointer-events-auto">
           <Image
             src="/logos/epitech.svg"
             alt="Epitech's logo"
