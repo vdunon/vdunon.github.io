8990a0e9aac2cc6547e0f64e8eba3507cd880582
Valentin DUNON <valentin.dunon@epitech.eu>
2026-05-26 20:57:59 +0200

    feat: finished skills categories tab animation

--- app/components/skillTabsRow.tsx
+++ app/components/skillTabsRow.tsx
@@ -1,13 +1,27 @@
-import SkillTab from "./skillTab"
+import "../css/marquee.css";
 
-export default function SkillTabsRow()
+function SkillTab({ title, key }: {title: string, key:string}) {
+    return (
+        <li className="inline-block w-[180px] flex-shrink-0 cursor-pointer bg-(image:--gradient-main) p-px rounded-lg" key={key}>
+            <div className="transition hover:bg-(image:--gradient-main) text-[#9153E3] hover:text-white bg-background rounded-lg p-2 h-full">
+                <h4 className="flex flex-col text-xl text-center">{title}</h4>
+            </div>
+        </li>
+    )
+}
+export default function SkillTabsRow({skills, toRight=false}:
+    {skills: string[], toRight?: boolean})
 {
-    const skillCategories = [
-        "Web", "Programming", "Databases", "DevOps", "OS", "Tools"
-    ] as const;
-    let skillItems = skillCategories.map((v) => SkillTab({ title: v }));
+    const skillItems = skills.map((v, i) => SkillTab({ title: v, key: "skills-1-" + i}));
+    const skillItems2 = skills.map((v, i) => SkillTab({ title: v, key: "skills-2-" + i}));
+    const animation = toRight ? "marquee-to-right" : "marquee-to-left";
 
     return (
-        <ul>{skillItems}</ul>
+        <div className="marquee-container w-full overflow-hidden flex select-none gap-2">
+            <div className={`flex flex-row gap-2 ${animation}`}>
+                <ul className={`flex flex-row gap-2`}>{skillItems}</ul>
+                <ul className={`flex flex-row gap-2`} aria-hidden="true">{skillItems2}</ul>
+            </div>
+        </div>
     )
 }
--- app/sections/skills.tsx
+++ app/sections/skills.tsx
@@ -8,7 +8,15 @@ export default function SkillsSection() {
         <section>
             <SectionTitle title={"Skills"}/>
             <h3 className="text-gray-600 mb-10 mt-5 text-center">{subtitle}</h3>
-            <SkillTabsRow/>
+            <div className="flex flex-col gap-y-2">
+                <SkillTabsRow
+                    skills={["Web", "Programming", "Databases", "DevOps", "OS", "Tools"]}
+                />
+                <SkillTabsRow
+                    skills={["DevOps", "OS", "Tools", "Web", "Programming", "Databases"]}
+                    toRight={true}
+                />
+            </div>
         </section>
     )
 }
